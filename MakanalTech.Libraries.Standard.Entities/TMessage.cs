0bc60e4e0102180e152412be9eb9e4603cc83158
Kane McConnell <kane@makanal.eu>
2020-03-12 12:47:08 +0100

    Add IMessage

---
+++ MakanalTech.Libraries.Standard.Entities/TMessage.cs
@@ -0,0 +1,16 @@
+﻿namespace MakanalTech.Libraries.Standard.Entities
+{
+    /// <summary>
+    /// Discrete unit of communication intended by the source for consumption
+    /// by some recipient or group of recipients.
+    /// </summary>
+    /// <typeparam name="T"></typeparam>
+    public abstract class TMessage<T> : TEntity<T>, IMessage<T>
+    {
+        public TMessage(string appellation) : base(appellation)
+        {
+        }
+
+        public string Body { get; set; }
+    }
+}
