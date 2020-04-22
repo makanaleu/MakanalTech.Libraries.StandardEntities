88131071840eb8db3e4455acb8b101b777d5a42b
Kane McConnell <kane@makanal.eu>
2020-04-22 20:12:42 +0200

    Add Reference and Type to TMessage

--- MakanalTech.Libraries.Standard.Entities/IMessageEditable.cs
+++ MakanalTech.Libraries.Standard.Entities/IMessageEditable.cs
@@ -6,5 +6,22 @@
         /// Free-form text of the message.
         /// </summary>
         string? Body { get; set; }
+
+        /// <summary>
+        /// Refers to the name of another entity to which the message is
+        /// associated.
+        /// </summary>
+        string? Reference { get; set; }
+
+        /// <summary>
+        /// Refers to the Id of the reference entity to which the message is
+        /// associated.
+        /// </summary>
+        object? ReferenceId { get; set; }
+
+        /// <summary>
+        /// Type of message (e.g. warning, errror, success)
+        /// </summary>
+        string? Type { get; set; }
     }
 }
